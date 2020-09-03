9ac8023bfad9ba94e7b8c9290bca829c68d3d67f
kamasu paul <kamasupaul@gmail.com>
2020-09-03 09:15:26 +0300

    disabled requiring auth for books routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::post('/logout', "AuthController@logout");
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->group(function () use ($router) {
+// Route::middleware('auth:api')->group(function () use ($router) {
     $router->get("books", "BooksController@showAllBooks");
     $router->get("books/{id}/videos", "BooksController@getVideos");
     $router->get("books/{id}", "BooksController@showOneBook");
@@ -29,7 +29,7 @@ Route::middleware('auth:api')->group(function () use ($router) {
     $router->delete("books/{id}", "BooksController@deleteBook");
     $router->put("books/{id}", "BooksController@updateBook");
     $router->get("sendmessage", "Controller@sendMessage");
-});
+// });
 
 Route::middleware('auth:api')->group(function () use ($router) {
     Route::resource('video', 'VideoController');
